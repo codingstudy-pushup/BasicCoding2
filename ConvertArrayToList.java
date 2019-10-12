190a7a8ee1052f5503f21c54ec8c606b787e2aa7
codingstudy-pushup <52611625+codingstudy-pushup@users.noreply.github.com>
2019-10-13 08:24:44 +0900

    Update ConvertArrayToList.java

--- ConvertArrayToList.java
+++ ConvertArrayToList.java
@@ -1,13 +1,38 @@
+package Basic;
+
 import java.util.*;
 
-public class ListTest {
+public class ArrayConvert {
 
 	public static void main(String[] args) {
 		int[][] nums = { { 1, 2 }, { 2, 3 } };
-		System.out.println(convert_ArrayTolist(nums));
-		List<List<Integer>> result = convert_ArrayTolist(nums);
-		int[][] array = convert_ListToArray(result);
+		int[] nums1 = { 1, 2 };
+		System.out.println(convert_ArrayTolist_1(nums1));
+//		List<List<Integer>> result = convert_ArrayTolist(nums);
+//		int[][] array = convert_ListToArray(result);
 	}
+
+	// 1 array-> list
+	private static List<Integer> convert_ArrayTolist_1(int[] nums) {
+		List<Integer> result = new ArrayList<>();
+
+		for (int i = 0; i < nums.length; i++) {
+			result.add(nums[i]);
+		}
+		System.out.println(result);
+		return result;
+	}
+
+	// 1 list -> array
+	private static int[] convert_ArrayToList(List<Integer> list) {
+		int[] array = new int[list.size()];
+		for (int i = 0; i < list.size(); i++) {
+			array[i] = list.get(i);
+		}
+		return array;
+	}
+
+	// 2 array-> list
 	private static List<List<Integer>> convert_ArrayTolist(int[][] nums) {
 		List<List<Integer>> result = new ArrayList<>();
 
@@ -20,29 +45,32 @@ public class ListTest {
 		}
 		return result;
 	}
-	
-	private static int[][] convert_ListToArray(List<List<Integer>> list){
+	// 2  list -> array
+	private static int[][] convert_ListToArray(List<List<Integer>> list) {
 		int[][] array = new int[list.size()][];
-		
-		for(int i=0; i<array.length; i++) {
+
+		for (int i = 0; i < array.length; i++) {
 			array[i] = new int[list.get(i).size()];
 		}
-		for(int i=0; i<list.size(); i++) {
-			for(int j=0; j<list.get(i).size(); j++) {
+		for (int i = 0; i < list.size(); i++) {
+			for (int j = 0; j < list.get(i).size(); j++) {
 				array[i][j] = list.get(i).get(j);
 			}
 		}
 		return array;
 	}
-	
-	List<int[]> intArrayList = new ArrayList();
+	// 2  list -> List<int[]>
+	private static List<int[]> convert_ListInArray(List<List<Integer>> list) {
+		List<int[]> intArrayList = new ArrayList<>();
 
-	for(List<Integer> intList : intListList){
-    		int[] intArray = new int[intList.size()];
-    		for(int i = 0; i < intArray.length; ++i){
-      		 intArray[i] = intList.get(i);
-   	 }
-    		intArrayList.add(intArray);
-}
-	
+		for (List<Integer> intList : list) {
+			int[] intArray = new int[intList.size()];
+			for (int i = 0; i < intArray.length; ++i) {
+				intArray[i] = intList.get(i);
+			}
+			intArrayList.add(intArray);
+		}
+		return intArrayList;
+
+	}
 }
