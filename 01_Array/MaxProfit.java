48ab6d26d0cb37ce9163540b09848c924b5ab6a9
codingstudy-pushup <52611625+codingstudy-pushup@users.noreply.github.com>
2020-12-10 00:33:53 +0900

    Update MaxProfit.java

--- 01_Array/MaxProfit.java
+++ 01_Array/MaxProfit.java
@@ -1,1 +1,25 @@
 
+package ama001;
+
+public class A055_BestTimeToBuyandSellStock {
+
+	public static void main(String[] args) {
+		int[] prices = {7,1,5,3,6,4};
+		System.out.println(maxProfit(prices));
+	}
+	public static int maxProfit(int[] prices) {
+		 if (prices.length == 0) {
+			 return 0 ;
+		 }		
+		 int max = 0 ;
+		 int sofarMin = prices[0] ;
+	     for (int i = 0 ; i < prices.length ; ++i) {
+	    	 if (prices[i] > sofarMin) {
+	    		 max = Math.max(max, prices[i] - sofarMin) ;
+	    	 } else{
+	    		sofarMin = prices[i];  
+	    	 }
+	     }	     
+	    return  max ;
+	 }
+}
