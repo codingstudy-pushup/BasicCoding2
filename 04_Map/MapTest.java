20ceb387136b8afaf9e47ed865c230989795c6c9
codingstudy-pushup <52611625+codingstudy-pushup@users.noreply.github.com>
2020-12-14 01:46:10 +0900

    Update MapTest.java

--- 04_Map/MapTest.java
+++ 04_Map/MapTest.java
@@ -1,26 +1,33 @@
-public static void main(String[] args) {
-   Map<string, string> map = new HashMap<string, string>();
-   map.put("key1", "value1"); 
-   map.put("key2", "value2"); 
-   map.put("key3", "value3"); 
-   map.put("key4", "value4"); 
-  
-    // 1. keySet
-    for( String key : map.keySet() ){ 
-    System.out.println( "key : " + key + ", value : " + map.get(key)) );
-    } 
-   
-   // 2. entrySet
-   for( Map.Entry<string, string> elem : map.entrySet() ){
-    System.out.println( "key : " + elem.getKey() + ", value : " + elem.getValue()) ); } 
-    
-  
-    
-    // 3 : Iterator를 이용한 방법
-    Iterator<string> keys = map.keySet().iterator(); 
-      while( keys.hasNext() ){ 
-        String key = keys.next(); 
-        System.out.println( "key : " + key + ", value : " + map.get(key)) ); 
-      }
-    }
+package Map;
 
+import java.util.*;
+
+public class MapTest {
+
+	
+	public static void main(String[] args) {
+		
+		Map<Integer, String> map = new HashMap<>();
+		map.put(1, "A");
+		map.put(2, "B");
+		map.put(3, "C");
+		
+		//1
+		for(Integer key : map.keySet()) {
+			System.out.println("key: "+key+" values "+map.get(key));
+		}
+		System.out.println();
+		//2 entrySet
+		for(Map.Entry<Integer, String> elem : map.entrySet()) {
+			System.out.println("key: "+elem.getKey()+" value "+elem.getValue());
+		}
+		//3
+		Iterator<Integer> keys = map.keySet().iterator();
+			while(keys.hasNext()) {
+				Integer key = keys.next();
+				System.out.println("key "+key);
+			}
+		
+		
+	}
+}
