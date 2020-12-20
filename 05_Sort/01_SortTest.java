398f6d135f2eef9e9cb0d1e967215977ae37c10e
codingstudy-pushup <52611625+codingstudy-pushup@users.noreply.github.com>
2020-12-21 08:01:30 +0900

    Create 01_SortTest.java

---
+++ 05_Sort/01_SortTest.java
@@ -0,0 +1,13 @@
+package Sort;
+
+public class SortTest {
+
+	public static void main(String[] args) {
+	  Integer  a = 1, b=3;
+		
+    System.out.println( a.compareTo(b) );  // -1
+    System.out.println( a.compareTo(1) );  //  0
+    System.out.println( a.compareTo(0) );  //  1
+		
+	}
+}
