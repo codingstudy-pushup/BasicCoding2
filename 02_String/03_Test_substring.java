a82eaf11d851381792a95dab76cc79040345f3d2
codingstudy-pushup <52611625+codingstudy-pushup@users.noreply.github.com>
2021-01-08 10:39:38 +0900

    Update 03_Test_substring.java

--- 02_String/03_Test_substring.java
+++ 02_String/03_Test_substring.java
@@ -1,23 +1,27 @@
+package String;
 
-public class Test02 {
+public class Test_charAt_03 {
 
 	public static void main(String[] args) {
-		String str[] = {"fltow","flower"};
-		System.out.println(new Test02().solve(str));
+		String str[] = { "test", "teacher" };
+		System.out.println(new Test_charAt_03().solve(str));
+		
 	}
-	
+
+
 	public String solve(String[] strs) {
-		if(strs.length==0) return "";
-		String pre = strs[0];
-		for(int i=1; i<strs.length; i++) {
-			System.out.println("pre: "+pre);
-			System.out.println("strs[i].indexOf(pre): "+strs[i].indexOf(pre));
-			while(strs[i].indexOf(pre)!=0) {
-				System.out.println(strs[i].indexOf(pre));
-				pre = pre.substring(0, pre.length()-1);
-				System.out.println("pre "+pre);
+		if (strs.length == 0)
+			return "";
+		String firstStr = strs[0];
+		for (int i = 1; i < strs.length; i++) {
+			
+			while (strs[i].indexOf(firstStr) != 0) {
+			
+				firstStr = firstStr.substring(0, firstStr.length() - 1);
+		
 			}
+		
 		}
-		return pre;
+		return firstStr;
 	}
 }
