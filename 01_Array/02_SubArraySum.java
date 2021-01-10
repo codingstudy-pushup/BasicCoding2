4b65345af5e3ed007ed6e4f79323595dd10a2a57
codingstudy-pushup <52611625+codingstudy-pushup@users.noreply.github.com>
2021-01-11 00:55:38 +0900

    Update 02_SubArraySum.java

--- 01_Array/02_SubArraySum.java
+++ 01_Array/02_SubArraySum.java
@@ -9,50 +9,30 @@ public class SubArraySum {
 		int[] nums = { 3, 4, 7, 2, -3, 1, 4, 2 };
 		int k = 7;
 		System.out.println(subarraySum(nums, k));
-		System.out.println(subarraySum2(nums, k));
+//		System.out.println(subarraySum_map(nums, k));
+
 	}
-//방법1
-// Time complexity : O(n^2)
-// Space complexity : O(1)
-	public static int subarraySum2(int[] nums, int k) {
+
+//  1. 이중 for
+	public static int subarraySum(int[] nums, int k) {
 		int count = 0;
-		for (int start = 0; start < nums.length; start++) {
-			int sum = 0;
-			for (int end = start; end < nums.length; end++) {
-				System.out.println("00start: " + start + " end: " + end + " sum: " + sum);
-				sum += nums[end];
-				if (sum == k) {
-					System.out.println("start: " + start + " end: " + end + " sum: " + sum + " k:" + k);
+
+		for(int i=0; i<nums.length; i++) {
+			int sum =0;
+			for(int j=i; j<nums.length;j++) {
+				sum = sum +nums[j];
+	
+				if(sum==k) {
+					System.out.println("sum "+sum+" nums["+j+"]"+nums[j]);
 					count++;
 				}
 			}
 		}
-		return count;
-	}
-	//방법2
-// Time complexity : O(n)
-// Space complexity : O()
-	public static int subarraySum(int[] nums, int k) {
-		if (nums == null || nums.length == 0) {
-			return -1;
-		}
-
-		Map<Integer, Integer> map = new HashMap<>();
-
-		map.put(0, 1); //
-		int sum = 0;
-		int count = 0;
-
-		for (int i = 0; i < nums.length; i++) {
-			sum += nums[i];
-			if (map.containsKey(sum - k)) {
-				System.out.println("sum-k "+(sum-k));
-				count += map.get(sum - k);
-			}
-			map.put(sum, map.getOrDefault(sum, 0) + 1);
-		}
-
+		
+		
+		
 		return count;
 	}
 
 }
+
