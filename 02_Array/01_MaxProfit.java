573f1fd7fd88612399116b5ac91d7f194a6d6d78
codingstudy-pushup <52611625+codingstudy-pushup@users.noreply.github.com>
2021-01-11 01:35:32 +0900

    Create 01_MaxProfit.java

---
+++ 02_Array/01_MaxProfit.java
@@ -0,0 +1,30 @@
+package Array;
+
+public class MaxProfit {
+
+	public static void main(String[] args) {
+		int[] prices = { 8, 2, 6, 5, 1, 7, 5 };
+		System.out.println(maxProfit(prices));
+	}
+
+	public static int maxProfit(int[] prices) {
+		if (prices.length == 0) {
+			return 0;
+		}
+		//1
+		int max = 0;
+		int sofarMin = prices[0];
+		
+		//2
+		// 8에 샀으면 버린다. 2에 샀으면
+		for (int i = 0; i < prices.length; ++i) {
+			if (prices[i] < sofarMin) {
+				sofarMin = prices[i];
+			} else {
+				max = Math.max(max, prices[i] - sofarMin);
+
+			}
+		}
+		return max;
+	}
+}
