e1969e6848a5886f54be9061d8f86673d45c2d50
codingstudy-pushup <52611625+codingstudy-pushup@users.noreply.github.com>
2021-01-18 16:01:35 +0900

    Update 05_map_pq.java

--- 05_Sort/05_map_pq.java
+++ 05_Sort/05_map_pq.java
@@ -13,42 +13,42 @@ public class TopK {
 		
 	}
 	public List<String> solve(String[] words, int k) {
+	//	1
+		List<String> result = new ArrayList<>();
+		Map<String, Integer> map = new HashMap<>();
 		
-	    List<String> result = new ArrayList<>();
-		HashMap<String, Integer> map = new HashMap<>();
-
-        for (String w: words) {
-            map.put(w, map.getOrDefault(w, 0) + 1);
-         
-        }
-       //1. PriorityQueue 람다식
-       PriorityQueue<Map.Entry<String, Integer>> pq = new PriorityQueue<>(
-                (a,b) -> a.getValue()==b.getValue() ? a.getKey().compareTo(b.getKey()) : b.getValue()-a.getValue()
-       );
-       
-       
-       //2 
-       Comparator<Map.Entry<String, Integer>> comp = new Comparator<Map.Entry<String, Integer>>() {
-           @Override
-           public int compare(Map.Entry<String, Integer> a, Map.Entry<String, Integer> b) {
-               if(a.getValue()==b.getValue())
-                   return (a.getKey().compareTo(b.getKey()));
-               return b.getValue().compareTo(a.getValue());
-           }
-       };
-       PriorityQueue<Map.Entry<String, Integer>> pq2 = new PriorityQueue<>(comp);
-
-
-      
-       
-       for(Map.Entry<String, Integer> entry: map.entrySet())
-           pq2.offer(entry);
-       
-       
-       while(k>0){
-           result.add(pq2.poll().getKey());
-           k --;
-       }
+		//1.map
+		for(String w : words) {
+			map.put(w, map.getOrDefault(w, 0)+1); //a3 b2 c2
+		}
+		
+		//2.pq
+		Queue<Map.Entry<String, Integer>> pq = new PriorityQueue<>((a,b)->
+		a.getValue()==b.getValue() ? a.getKey().compareTo(b.getKey()) : 
+		b.getValue()-a.getValue());
+		
+		
+		//2.pq comparator
+		Comparator<Map.Entry<String, Integer>> comp = new Comparator<Map.Entry<String, Integer>>() {
+			@Override
+			public int compare(Map.Entry<String, Integer> a, Map.Entry<String, Integer> b) {
+				if(a.getValue()==b.getValue())
+					return a.getKey().compareTo(b.getKey());
+				return b.getValue()-a.getValue();
+			}
+		};
+		Queue<Map.Entry<String, Integer>> pq2 = new PriorityQueue<>(comp);
+		
+		
+		
+		for(Map.Entry<String, Integer> entry : map.entrySet()) {
+			pq2.offer(entry);
+		}
+		
+		while(k>0) {
+			result.add(pq2.poll().getKey());
+			k--;
+		}
        return result;
     }
 }
