462d6c5e3295b70f65b45dbbcaa93652ffcf6d28
codingstudy-pushup <52611625+codingstudy-pushup@users.noreply.github.com>
2021-01-18 11:28:50 +0900

    Create 04_Map_pq.java

---
+++ 03_Sort/04_Map_pq.java
@@ -0,0 +1,54 @@
+package Sort;
+import java.util.*;
+import java.util.Map.Entry;
+
+public class TopK {
+	private static final Comparator<? super Entry<String, Integer>> Comp = null;
+	public static void main(String[] args) {
+//		String[] words= {"i", "study", "inflearn", "i", "study", "coding"};
+		int k = 2;
+		String[] words= {"a", "b", "c", "a", "b", "c"};
+		TopK a = new TopK();
+		System.out.println(a.solve(words, k));
+		
+	}
+	public List<String> solve(String[] words, int k) {
+		
+	    List<String> result = new ArrayList<>();
+		HashMap<String, Integer> map = new HashMap<>();
+        int max = 0;
+        for (String w: words) {
+            map.put(w, map.getOrDefault(w, 0) + 1);
+            max = Math.max(max, map.get(w));
+        }
+       //1. PriorityQueue 람다식
+       PriorityQueue<Map.Entry<String, Integer>> pq = new PriorityQueue<>(
+                (a,b) -> a.getValue()==b.getValue() ? b.getKey().compareTo(a.getKey()) : a.getValue()-b.getValue()
+       );
+       
+       
+       //2 
+       Comparator<Map.Entry<String, Integer>> comp = new Comparator<Map.Entry<String, Integer>>() {
+           @Override
+           public int compare(Map.Entry<String, Integer> a, Map.Entry<String, Integer> b) {
+               if(a.getValue()==b.getValue())
+                   return (a.getKey().compareTo(b.getKey()));
+               return b.getValue().compareTo(a.getValue());
+           }
+       };
+       PriorityQueue<Map.Entry<String, Integer>> pq2 = new PriorityQueue<>(comp);
+
+
+      
+       
+       for(Map.Entry<String, Integer> entry: map.entrySet())
+           pq2.offer(entry);
+       
+       
+       while(k>0){
+           result.add(pq2.poll().getKey());
+           k --;
+       }
+       return result;
+    }
+}
