684992d7a35f34984dd5bb5a657f744c457082d4
codingstudy-pushup <52611625+codingstudy-pushup@users.noreply.github.com>
2021-01-31 13:18:41 +0900

    Update 02_toCharArray.java

--- 01_String/02_toCharArray.java
+++ 01_String/02_toCharArray.java
@@ -6,34 +6,44 @@ public class Test_charAt_02 {
 public static void main(String[] args) {
 		
 //		String s = "(a(b(c)d)";
+//		String s = "(((a(b(c(e(f)d))";
 //	    String  s = "in(f(lea)r)n)";
-		String  s = "a)b(c)d";
+//		String  s = "a)b(c)d";
+		String  s = "a)b(c)d)";
+
+//		String  s = "(()))";
+//		String  s = "(()";
+//	    String  s = "))((";
 		System.out.println(new Test_charAt_02().solve(s));
 	}
 	public String solve(String s) {
+		//1
 		StringBuilder sb = new StringBuilder();
-		int count =0;
-		
-		for(char c :s.toCharArray()) {
-			System.out.println("c: "+c);
-			if(c=='(') {
-				count++;
-			}else if(c==')') {
-				if(count==0) continue;
-				count--;
-			}
-			sb.append(c);
-		}
-		
 		StringBuilder result = new StringBuilder();
-		for(int i =sb.length()-1; i >=0; i-- ) {
-			if(sb.charAt(i) =='(' && count-->0 ) continue;
-			result.append(sb.charAt(i));
-			System.out.println("result "+result);
-
-		}
-		 return result.reverse().toString();
+		int openBrace=0; // '('
 		
+		//2 count 
+		for(char c: s.toCharArray()) {
+			System.out.println("c "+c);
+			if(c=='(') {
+				openBrace++;
+			}else if(c==')') {
+				if(openBrace==0) continue;
+				openBrace--;
+			}
+			sb.append(c);//그대로 저장
+		}
+
+		System.out.println("1_sb== "+sb);
+		//3 제외 할 대상만 빼고 그대로 저장
+		for(int i=sb.length()-1; i>=0; i--) {
+			System.out.println("sb "+sb.charAt(i));
+			if(sb.charAt(i)=='(' && openBrace-- > 0) {
+				continue;
+			}
+			result.append(sb.charAt(i));
+		}
+		return result.reverse().toString();
 		
 	}
 }
