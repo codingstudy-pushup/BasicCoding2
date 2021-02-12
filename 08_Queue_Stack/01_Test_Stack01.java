1616fa0476dce60cb79740805262e257822bff8c
codingstudy-pushup <52611625+codingstudy-pushup@users.noreply.github.com>
2021-02-12 12:03:25 +0900

    Update 01_Test_Stack01.java

--- 08_Queue_Stack/01_Test_Stack01.java
+++ 08_Queue_Stack/01_Test_Stack01.java
@@ -1,1 +1,59 @@
+package Stack_Queue;
 
+import java.util.*;
+
+class TreeNode {
+	int val;
+	TreeNode left, right;
+
+	TreeNode(int x) {
+		this.val = x;
+	}
+}
+
+public class Test_Stack01 {
+
+	public static void main(String[] args) {
+		TreeNode root = new TreeNode(1);
+		root.left = new TreeNode(2);
+		root.right = new TreeNode(3);
+		root.left.left = new TreeNode(4);
+		root.left.right = new TreeNode(5);
+
+		System.out.println(solve(root));
+	}
+
+	public static List<List<Integer>> solve(TreeNode root) {
+
+		List<List<Integer>> result = new ArrayList<>();
+		Queue<TreeNode> queue = new LinkedList<>();
+		queue.offer(root);
+		boolean zigzag = true;
+
+		while (!queue.isEmpty()) {
+			int size = queue.size();
+			List<Integer> list = new ArrayList<>();
+
+			for (int i = 0; i < size; i++) {
+				TreeNode node = queue.poll();
+				if (zigzag) {
+					list.add(node.val);
+				} else {
+					list.add(0, node.val);
+				}
+
+				if (node.left != null) {
+					queue.offer(node.left);
+				}
+				if (node.right != null) {
+					queue.offer(node.right);
+				}
+
+			}
+			zigzag = !zigzag;
+			result.add(list);
+		}
+		return result;
+	}
+
+}
