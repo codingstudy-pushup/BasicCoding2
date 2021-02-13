cb1558670587929de24ee046e4e29b5162fc396c
codingstudy-pushup <52611625+codingstudy-pushup@users.noreply.github.com>
2021-02-13 12:05:51 +0900

    Update 01_Permutaion.java

--- 09_BackTracking/01_Permutaion.java
+++ 09_BackTracking/01_Permutaion.java
@@ -2,37 +2,42 @@ package Permutation;
 
 import java.util.*;
 
-public class Permutaion {
+public class Permutation {
 
 	public static void main(String[] args) {
 		int[] nums = { 1, 2, 3 };
-		System.out.println(new Per().solve(nums));
+		System.out.println(new Permutation().solve(nums));
 	}
 
 	public List<List<Integer>> solve(int[] nums) {
-		List<List<Integer>> list = new ArrayList<>();
+		List<List<Integer>> result = new ArrayList<>();
 		List<Integer> tempList = new ArrayList<>();
-		backtrack(list, tempList, nums);
-		return list;
+		backtrack(result, tempList, nums);
+		return result;
 	}
 	int count =0;
-	private void backtrack(List<List<Integer>> list, List<Integer> tempList, int[] nums) {
-		System.out.println("tempList: "+tempList);
-		count++;
-		System.out.println("count "+count);
+	private void backtrack(List<List<Integer>> result, List<Integer> tempList, int[] nums) {
+//		System.out.println("tempList: "+tempList);
+//		count++;
+//		System.out.println("count "+count);
 		if (tempList.size() == nums.length) {
-			list.add(new ArrayList<>(tempList));
+			result.add(new ArrayList<>(tempList));
 		} else {
 			for (int i = 0; i < nums.length; i++) {
-				if (tempList.contains(nums[i]))
+			//	System.out.println("11tempList: "+tempList+" nums["+i+"]:"+nums[i]);	
+				if (tempList.contains(nums[i])) {
+			//	System.out.println("12tempList: "+tempList+" nums["+i+"]:"+nums[i]);	
 					continue; 
-				System.out.println("i "+i);
+				}
+					
+		//		System.out.println("i "+i);
 				tempList.add(nums[i]);
-				System.out.println("22tempList: "+tempList);
-				backtrack(list, tempList, nums);
+		//		System.out.println("22tempList: "+tempList);
+				backtrack(result, tempList, nums);
 				tempList.remove(tempList.size() - 1);
-				System.out.println("33tempList: "+tempList);
+		//		System.out.println("33tempList: "+tempList);
 			}
 		}
+		//System.out.println("====end====================="+count );
 	}
 }
