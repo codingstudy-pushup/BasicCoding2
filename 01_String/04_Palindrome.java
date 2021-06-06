17c0acf76101af94e1c52c8ca76d24113731b9ee
codingstudy-pushup <52611625+codingstudy-pushup@users.noreply.github.com>
2021-06-06 14:52:17 +0900

    Update 04_Palindrome.java

--- 01_String/04_Palindrome.java
+++ 01_String/04_Palindrome.java
@@ -6,8 +6,8 @@ public class String_palindrom {
 		String_palindrom a = new String_palindrom();
 //		String s = "b";		
 //		String s = "bb";
-		String s = "bbb";
-//		String s = "bbbb";
+//		String s = "bbb";
+		String s = "bbbb";
 		
 //		String s = "abba";
 //		String s = "banana";
@@ -19,33 +19,41 @@ public class String_palindrom {
 	}
 
 	int start, end;
-
 	public String solve(String s) {
+		//1. ds
 		int len = s.length();
-		if (len < 2) return s;
+		if(len <2) return s;
+
 		
-		for(int i=0; i<len-1; i++) {
-			System.out.println("i: "+i);
+		//2. for while
+		for(int i=0; i<len; i++) {
+			System.out.println("i "+i);
+//			if(i==3) {
 			findSubstring(s,i,i);//odd
 			findSubstring(s,i,i+1);//even
-		}
-		return s.substring(start , start+end);
 
-	}
-	public void findSubstring(String s, int left, int right) {
-		
-		System.out.println("l: "+left+" r: "+right);
-		while(0 <= left && right<s.length() && s.charAt(left) == s.charAt(right)) {
-			left--;
-			right++;
+//			}
 			
 		}
-		if(end< right-left-1) {
-			end =right-left-1;//6
-			start = left+1;//0
-		}
+		return s.substring(start, end+start);
 		
 	}
+	public void findSubstring(String s,int left, int right) {
+	
+		while( 0<=left&& right<s.length() && s.charAt(left) == s.charAt(right)) {
+			left--;//2,1,0
+			right++;//4,5,6
+			System.out.println("left "+left+" right "+right);
+		}
+		//0,6 => 1,5
+		if( end< right-left-1) {
+			start=left+1;
+		    end= right-left-1;
+		}
+		System.out.println("start "+start+" end "+end);
+	}
+	
+	
 
 
 }
